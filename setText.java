ffc01e24f0b8c3f00a3aabb4fced75e06b21ce7e
ho1onk <hogyzhnc@gmail.com>
2014-12-19 07:48:17 +0700

    Create setText.java

---
+++ setText.java
@@ -0,0 +1,5 @@
+//buat sebuah textview dengan id tampil di layout
+//code ini sisipkan setelah onCretae atau bisa juga onClick
+
+TextView teks = (TextView) findViewById(R.id.tampil);
+teks.setText("Halo wkwkwkwk");
